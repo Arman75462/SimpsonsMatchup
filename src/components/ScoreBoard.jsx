7159dd79d72b1fb543b5e270892e542284daf199
Arman <armangulistani@gmail.com>
2024-04-23 08:55:56 -0400

    Completed ScoreBoard component

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
+import "../styles/componentsStyles/ScoreBoard.css";
 
 function ScoreBoard() {
-  return <div className="ScoreBoard"></div>;
+  return (
+    <div className="ScoreBoard">
+      <p className="ScoreBoard__score">
+        Score: <span className="ScoreBoard__score-value">0</span>
+      </p>
+      <p className="ScoreBoard__best-score">
+        Best score: <span className="ScoreBoard__best-score-value">0</span>
+      </p>
+    </div>
+  );
 }
 
 export default ScoreBoard;
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./../styles/pagesStyles/Header.css";
 import SimpsonsLogo from "../components/Logo.jsx";
+import ScoreBoard from "../components/ScoreBoard.jsx";
 
 function Header() {
   return (
     <header className="Header">
       <SimpsonsLogo />
+      <ScoreBoard />
     </header>
   );
 }
